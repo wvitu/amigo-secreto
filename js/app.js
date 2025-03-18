6731b1a18ce5bc8ba2195ce6e932284ca6c93022
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-03-18 16:39:20 -0300

    Funções criadas
    Função soetear criada

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let amigos = []
+
+function adicionar () {
+    let amigo = document.getElementById('nome-amigo')
+    let lista = document.getElementById('lista-amigos')
+
+    amigos.push(amigo.value)
+    if (lista.textContent == '') {
+    lista.textContent = amigo.value 
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value
+    }
+    amigo.value = ''
+}
+
+function sortear() {
+    embaralha(amigos)
+    let sorteio = document.getElementById('lista-sorteio')
+    
+    for (let i = 0; i < amigos.length; i ++) {
+        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>'
+    }
+
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
