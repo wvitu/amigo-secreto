7a64302300fa320a2070f95606b799a6dc4c804e
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-03-18 16:43:08 -0300

    Ativando o botão sorteio

--- js/app.js
+++ js/app.js
@@ -18,7 +18,14 @@ function sortear() {
     let sorteio = document.getElementById('lista-sorteio')
     
     for (let i = 0; i < amigos.length; i ++) {
-        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>'
+
+        if (i == amigos.length -1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>'
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>'
+        }
+
+        
     }
 
 }
